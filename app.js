ac45f87e5a0b9f31cf5c82f015b5e11fec61883d
Ponraj <110389942+Ponraj-dev@users.noreply.github.com>
2023-04-23 19:11:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,7 @@
-// const form =document.getElementById("chat-form")
-// const input =document.getElementById("chat-input")
-// const message = document.getElementById("chat-messages")
-// const apiKey ="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";
-
-// form.addEventListener("submit",async (e)=>
-// {
-//     e.preventDefault();
-//     const message =input.value;
-//     input.value="";
-
-//     message.innerHTML +=`<div class="message user-message"> 
-//     <img src='./icons/user.png' alt="user icon"<span>${message}</span>
-//     </div>`;
-
-//     const response = await axios.post(
-//         "https://api.openai.com/v1/completions",
-//         {
-//             prompt: message,
-//             model:"text-devinci-003",
-//             temperature:0,
-//             max_token:1000,
-//             top_p:1,
-//             frequency_penalty:0.0,
-//             presence_penalty:0.0,
-//         },
-//         {
-//             header:{
-//                 "content-type":"application/json",
-//                 Authorization: `Bearer  ${apiKey}`
-//             },
-//         }
-//     );
-//     const chatbotResponse =response.data.choices[0].text;
-
-
-//     message.innerHTML +=`<div class="message user-message"> 
-//     <img src='./icons/user.png' alt="user icon"<span>${chatbotResponse}</span>
-//     </div>`;
-
-
-
-// });
-
-
-
 const form = document.getElementById("chat-form");
 const input = document.getElementById("chat-input");
 const messages = document.getElementById("chat-messages");
-const apiKey = "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";
+const apiKey = "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";
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
